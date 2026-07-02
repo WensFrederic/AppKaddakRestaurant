9fec946e3373eec4956fe5479258feb2878fc784
josemiguel5755 <acostavazque133@gmail.com>
2026-07-01 23:51:27 -0400

    se elimino la crendenciales de la base de datos

---
+++ .angular/cache/20.3.27/app/vite/deps/p-D7bSXQD3-IXD5KMRO.js
@@ -0,0 +1,35 @@
+import {
+  c,
+  m
+} from "./chunk-TM4GKXEZ.js";
+import {
+  e
+} from "./chunk-Q6ZGL456.js";
+import {
+  H,
+  P
+} from "./chunk-5ZNTDABU.js";
+import {
+  __async
+} from "./chunk-WDMUDEB6.js";
+
+// node_modules/@ionic/core/components/p-D7bSXQD3.js
+var i = () => {
+  const i2 = window;
+  i2.addEventListener("statusTap", (() => {
+    H((() => {
+      const o = document.elementFromPoint(i2.innerWidth / 2, i2.innerHeight / 2);
+      if (!o) return;
+      const n = m(o);
+      n && new Promise(((o2) => e(n, o2))).then((() => {
+        P((() => __async(null, null, function* () {
+          n.style.setProperty("--overflow", "hidden"), yield c(n, 300), n.style.removeProperty("--overflow");
+        })));
+      }));
+    }));
+  }));
+};
+export {
+  i as startStatusTap
+};
+//# sourceMappingURL=p-D7bSXQD3-IXD5KMRO.js.map
